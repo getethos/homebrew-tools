0769e0b46179f2ca1626cc11c9e4b6e39422e646
Douglas Land <doug.land@getethos.com>
2020-01-09 01:05:33 -0600

    trying to force-install botocore

--- gimme-aws-creds.rb
+++ gimme-aws-creds.rb
@@ -76,6 +76,15 @@ class GimmeAwsCreds < Formula
 
     venv = virtualenv_create(libexec, "python3")
 
+    resource("botocore").stage do
+      # Without removing this file, `pip` will ignore the `setup.py` file and
+      # attempt to download the [`flit`](https://github.com/takluyver/flit)
+      # build system.
+      rm_f "pyproject.toml"
+
+      venv.pip_install Pathname.pwd
+    end
+
     resource("boto3").stage do
       # Without removing this file, `pip` will ignore the `setup.py` file and
       # attempt to download the [`flit`](https://github.com/takluyver/flit)
