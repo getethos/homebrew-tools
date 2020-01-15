f330f145f5b72fcc2dc50dfb1c5ea40558805854
Douglas Land <doug.land@getethos.com>
2020-01-15 11:12:48 -0600

    add dependency for devops/tools/gimme-aws-creds

--- auth-support.rb
+++ auth-support.rb
@@ -4,6 +4,7 @@ class AuthSupport < Formula
   url "https://github.com/getethos/auth-support/archive/v0.0.1.tar.gz"
   sha256 "a474cd4a39b09d2e084b019e92b98b6e1557630681839fac5a502c05a17129e2"
   depends_on "awscli"
+  depends_on "devops/tools/gimme-aws-creds"
 
   def install
     bin.install Dir["bin/*"]
